cb15fae60a11e2973a5f9dbf73e01115be50fd8a
harukaty04 <haruka.kimura04@gmail.com>
2020-12-13 15:10:38 +0900

    edit migrationfifle

--- server/database/migrations/2020_12_13_053005_create_tasks_table.php
+++ server/database/migrations/2020_12_13_053005_create_tasks_table.php
@@ -15,6 +15,10 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('folder_id')->unsigned();
+            $table->string('title', 100);
+            $table->date('due_date');
+            $table->integer('status')->default(1);
             $table->timestamps();
         });
     }
