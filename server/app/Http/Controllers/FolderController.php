3357b0dbb4231f8bf7681100417f3bedf797f07c
harukaty04 <haruka.kimura04@gmail.com>
2020-12-15 23:24:09 +0900

    edit FolderController.php to receive input value

--- server/app/Http/Controllers/FolderController.php
+++ server/app/Http/Controllers/FolderController.php
@@ -4,15 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Folder; 
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateFolder; 
 
 class FolderController extends Controller
 {
+    
     public function showCreateForm()
     {
         return view('folders/create');
     }
     //引数にインポートしたRequestクラスを受け入れる
-    public function create(Request $request)
+    public function create(CreateFolder $request)
     {
     // フォルダモデルのインスタンスを作成する
     $folder = new Folder();
