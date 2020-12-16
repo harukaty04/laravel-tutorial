e044e7dd29bf615f9e73eb2ebf6fca5f09929be9
harukaty04 <haruka.kimura04@gmail.com>
2020-12-16 23:45:31 +0900

    edit controller for displaying showEditForm

--- server/app/Http/Controllers/TaskController.php
+++ server/app/Http/Controllers/TaskController.php
@@ -66,4 +66,13 @@ public function create(int $id, CreateTask $request)
     ]);
 }
 
+public function showEditForm(int $id, int $task_id)
+{
+    $task = Task::find($task_id);
+
+    return view('tasks/edit', [
+        'task' => $task,
+    ]);
+}
+
 }
