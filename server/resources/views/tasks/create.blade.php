220ae4b7d0fd194858c1f43926a0b57d096285e4
harukaty04 <haruka.kimura04@gmail.com>
2020-12-17 21:45:19 +0900

    edit file for error correction

--- server/resources/views/tasks/create.blade.php
+++ server/resources/views/tasks/create.blade.php
@@ -1,29 +1,12 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ToDo App</title>
-    <!-- flatpickerを使用するためのファイル -->
-    
+
 @extends('layout')
 
 @section('styles')
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
-  <link rel="stylesheet" href="https://npmcdn.com/flatpickr/dist/themes/material_blue.css">
+@include('share.flatpickr.styles')
 @endsection
 
 @section('content')
-  <link rel="stylesheet" href="/css/styles.css">
-</head>
-<body>
-<header>
-  <nav class="my-navbar">
-    <a class="my-navbar-brand" href="/">ToDo App</a>
-  </nav>
-</header>
-<main>
+
 
 
   <div class="container">
@@ -58,20 +41,10 @@
       </div>
     </div>
   </div>
-</main>
+
 
 @endsection
 @section('scripts')
+@include('share.flatpickr.scripts')
 
-<script src="https://npmcdn.com/flatpickr/dist/flatpickr.min.js"></script>
-<script src="https://npmcdn.com/flatpickr/dist/l10n/ja.js"></script>
-<script>
-  flatpickr(document.getElementById('due_date'), {
-    locale: 'ja',
-    dateFormat: "Y/m/d",
-    minDate: new Date()
-  });
-</script>
 @endsection
-</body>
-</html> 
